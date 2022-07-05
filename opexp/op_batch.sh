f50e872201caca709112520bf5df74223596487c
arrenglover <arren.glover@iit.it>
2022-07-05 14:20:42 +0000

    fixed output folder structure batch_op

--- opexp/op_batch.sh
+++ opexp/op_batch.sh
@@ -1,10 +1,21 @@
 filenames=`ls /usr/local/HPE/data/H36m/_cam2_S9_videos/*.mp4`
 outpath='/usr/local/HPE/experiments/openpose_comparison/'
+algname='openpose_rgb.csv'
 for eachfile in $filenames
 do
-   outfile=${eachfile/.mp4/.csv}
-   outfile="$outpath${outfile##*/}"
-   if ! [ -f $outfile ]; then
-       openpose-evaluation $eachfile $outfile
-   fi
+    #this currently creates /outpath/datasetname.csv but we want
+    #                       /output/datasetname/algname.csv
+    #outfile=${eachfile/.mp4/.csv}
+
+    outfile=${eachfile%.*}
+    outdir="$outpath${outfile##*/}"
+    outfile="$outdir/$algname"
+    echo ${outdir}
+
+    if ! [ -d $outdir ]; then
+	    mkdir $outdir
+    fi
+    if ! [ -f $outfile ]; then
+        openpose-evaluation $eachfile $outfile
+    fi
 done
