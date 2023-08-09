40153bdabcfd87a398a26d90f51a950ae6864312
Arren Glover <arren.glover@iit.it>
2023-08-09 09:22:33 +0000

    low power changes

--- edpr-vojext-demo.cpp
+++ edpr-vojext-demo.cpp
@@ -8,8 +8,9 @@ Author: Franco Di Pietro, Arren Glover
 #include <event-driven/core.h>
 #include <event-driven/algs.h>
 #include <hpe-core/utility.h>
-#include <hpe-core/motion_estimation.h>
+#include <hpe-core/motion.h>
 #include <hpe-core/fusion.h>
+#include <hpe-core/representations.h>
 #include <opencv2/opencv.hpp>
 #include <vector>
 #include <string>
@@ -30,7 +31,7 @@ private:
     BufferedPort<Bottle> input_port;
 
 public:
-    bool init(std::string output_name, std::string input_name, double rate)
+    bool init(std::string output_name, std::string input_name, double period)
     {
         if (!output_port.open(output_name))
             return false;
@@ -38,7 +39,7 @@ public:
         if (!input_port.open(input_name))
             return false;
 
-        period = 1.0 / rate;
+        this->period = period;
         return true;
     }
     void close()
@@ -47,12 +48,15 @@ public:
         input_port.close();
     }
 
-    bool update(cv::Mat &latest_image, double latest_ts, hpecore::stampedPose &previous_skeleton)
+    bool update(const cv::Mat &latest_image, double latest_ts, hpecore::stampedPose &previous_skeleton)
     {
         // send an update if the timer has elapsed
         if ((!waiting && latest_ts - tic > period) || (latest_ts - tic > 2.0))
         {
-            output_port.prepare().copy(yarp::cv::fromCvMat<PixelMono>(latest_image));
+            cv::Mat temp;
+            latest_image.convertTo(temp, CV_8U);
+            cv::GaussianBlur(temp, temp, {5, 5}, -1);
+            output_port.prepare().copy(yarp::cv::fromCvMat<PixelMono>(temp));
             output_port.write();
             tic = latest_ts;
             waiting = true;
@@ -78,8 +82,7 @@ class VOJEXT_HPE : public RFModule
 
 private:
     // event reading
-    std::thread thread_events;
-    std::thread thread_detection;
+    std::thread thread_events, thread_detection, thread_rosimg;
     ev::window<ev::AE> input_events;
 
     // detection handlers
@@ -94,12 +97,13 @@ private:
 
     // internal data structures
     hpecore::skeleton13 skeleton_detection{0};
+    hpecore::pwtripletvelocity velocitizer;
 
     cv::Size image_size{640, 480};
     cv::Mat edpr_logo;
 
     // parameters
-    double th_period{0.01}, thF{100.0};
+    double p_vis{0.033}, p_img{0.2}, p_det{0.2}, p_vel{0.02};
     double tnow{0.0};
     bool vis{false};
 
@@ -112,6 +116,19 @@ private:
 public:
     bool configure(yarp::os::ResourceFinder &rf) override
     {
+        // =====SET UP YARP=====
+        if(rf.check("help")) {
+            yInfo() << " EDPR VOJEXT HPE ";
+            yInfo() << "--name <string> : name of module for YARP ports";
+            yInfo() << "--vis <bool>    : open visualisation";
+            yInfo() << "--f_vis <float> : visualisation rate [20]";
+            yInfo() << "--f_det <float> : HPE detection rate [5]";
+            yInfo() << "--f_vel <float> : HPE velocity estimation rate [50]";
+            yInfo() << "--f_img <float> : ROS image output rate [3]";
+            yInfo() << "--pu <float>    : KF process uncertainty [10.0]";
+            yInfo() << "--muD <float>   : KF measurement uncertainty [1.0]";
+            return false;
+        }
         // =====SET UP YARP=====
         if (!yarp::os::Network::checkNetwork(2.0))
         {
@@ -135,12 +152,16 @@ public:
         // =====READ PARAMETERS=====
         double procU = rf.check("pu", Value(10)).asFloat64();
         double measUD = rf.check("muD", Value(1)).asFloat64();
-        double measUV = rf.check("muV", Value(0)).asFloat64();
         
-        double moveEnet_f = rf.check("moveEnet_f", Value(30.0)).asFloat64();
-        double publish_f = rf.check("publish_f", Value(30.0)).asFloat64();
-        vis = rf.check("vis", Value(false)).asBool(); 
-        th_period = 1.0/publish_f;
+        vis = rf.check("vis", Value(true)).asBool();
+        p_vis = 1.0/std::max(rf.check("f_vis", Value(20.0)).asFloat64(), 5.0);
+        p_det = 1.0/std::max(rf.check("f_det", Value(5.0)).asFloat64(), 1.0);
+        p_vel = 1.0/std::max(rf.check("f_vel", Value(50)).asFloat64(), 1.0/p_det);
+        if(rf.check("f_img", Value(3.0)).asFloat64() > 0)
+            p_img = 1.0/rf.check("f_img", Value(3.0)).asFloat64();
+        else
+            p_img = 0.0;
+        
 
         // run python code for movenet
         int r = system("python3 /usr/local/src/hpe-core/example/movenet/movenet_online.py &");
@@ -154,7 +175,7 @@ public:
         yInfo() << killline;
         
 
-        if (!mn_handler.init(getName("/eros:o"), getName("/movenet:i"), moveEnet_f))
+        if (!mn_handler.init(getName("/eros:o"), getName("/movenet:i"), p_det))
         {
             yError() << "Could not open movenet ports";
             return false;
@@ -166,7 +187,7 @@ public:
         edpr_logo = cv::imread("/usr/local/src/edpr-vojext/edpr_logo.png");
 
         // fusion
-        if (!state.initialise({procU, measUD, measUV, -1.0}))
+        if (!state.initialise({procU, measUD, 0.0, -1.0}))
         {
             yError() << "Not KF initialized";
             return false;
@@ -216,6 +237,9 @@ public:
 
         thread_events = std::thread([this]{ this->run_camera_interface(); });
         thread_detection = std::thread([this]{ this->run_detection(); });
+        if(p_img > 0.0)
+            thread_rosimg = std::thread([this]{ this->run_rosimg(); });
+
 
         return true;
     }
@@ -224,7 +248,7 @@ public:
     {
         // run the module as fast as possible. Only as fast as new images are
         // available and then limited by how fast OpenPose takes to run
-        return th_period;
+        return p_vis;
     }
 
     bool interruptModule() override
@@ -234,7 +258,8 @@ public:
         mn_handler.close();
         thread_events.join();
         thread_detection.join();
-
+        if(p_img > 0.0)
+            thread_rosimg.join();
 
         int r = system(killline.c_str());
         
@@ -275,6 +300,45 @@ public:
     // synchronous thread
     bool updateModule() override
     {
+        
+        if(vis) {
+            if (cv::getWindowProperty("edpr-vojext", cv::WND_PROP_ASPECT_RATIO) < 0) {
+                stopModule();
+                return false;
+            }
+
+            static bool show_eros = true;
+            static cv::Mat canvas = cv::Mat(image_size, CV_8UC3, cv::Vec3b(0, 0, 0));
+
+            if(show_eros)
+                drawEROS_RGB(canvas);
+            else
+                drawEVENTS_RGB(canvas);
+
+            // plot skeletons
+            hpecore::drawSkeleton(canvas, state.query(), {0, 0, 255}, 3);
+
+            if (!edpr_logo.empty())
+            {
+                static cv::Mat mask;
+                cv::cvtColor(edpr_logo, mask, CV_BGR2GRAY);
+                edpr_logo.copyTo(canvas, mask);
+            }
+
+            cv::imshow("edpr-vojext", canvas);
+            char key = cv::waitKey(1);
+            if(key == '\e')
+                cv::destroyWindow("edpr-vojext");
+            else if(key == 'e')
+                show_eros = !show_eros; 
+        }
+        binary_handler.getSurface().setTo(0);
+        return true;
+    }
+
+    void run_rosimg()
+    {
+        while(!isStopping())
         {
             static yarp::os::Stamp ystamp;
             ystamp.update();
@@ -313,41 +377,9 @@ public:
             memcpy(rosEVS.data.data(), yarpEVS.getRawImage(), yarpEVS.getRawImageSize());
             publisherPort_evs.setEnvelope(ystamp);
             publisherPort_evs.write();
+
+            Time::delay(p_img);
         }
-
-        if(vis) {
-            if (cv::getWindowProperty("edpr-vojext", cv::WND_PROP_ASPECT_RATIO) < 0) {
-                stopModule();
-                return false;
-            }
-
-            static bool show_eros = true;
-            static cv::Mat canvas = cv::Mat(image_size, CV_8UC3, cv::Vec3b(0, 0, 0));
-
-            if(show_eros)
-                drawEROS_RGB(canvas);
-            else
-                drawEVENTS_RGB(canvas);
-
-            // plot skeletons
-            hpecore::drawSkeleton(canvas, skeleton_detection, {0, 0, 255}, 3);
-
-            if (!edpr_logo.empty())
-            {
-                static cv::Mat mask;
-                cv::cvtColor(edpr_logo, mask, CV_BGR2GRAY);
-                edpr_logo.copyTo(canvas, mask);
-            }
-
-            cv::imshow("edpr-vojext", canvas);
-            char key = cv::waitKey(1);
-            if(key == '\e')
-                cv::destroyWindow("edpr-vojext");
-            else if(key == 'e')
-                show_eros = !show_eros; 
-        }
-        binary_handler.getSurface().setTo(0);
-        return true;
     }
 
     void run_camera_interface()
@@ -366,39 +398,38 @@ public:
 
     void run_detection()
     {
-
         while (!isStopping())
         {
-        hpecore::stampedPose detected_pose;
-        cv::Mat eros8u; drawEROS_MONO(eros8u);
-        bool was_detected = mn_handler.update(eros8u, yarp::os::Time::now(), detected_pose);
+            Time::delay(p_vel);
+            hpecore::stampedPose detected_pose;
+            bool was_detected = mn_handler.update(eros_handler.getSurface(), tnow, detected_pose);
 
-        if(was_detected) {
-            if(!state.poseIsInitialised())
-                state.set(detected_pose.pose, detected_pose.timestamp);
-            else
-                state.updateFromPosition(detected_pose.pose, detected_pose.timestamp);
-            skeleton_detection = state.query();
-
+            if(was_detected) 
             {
+                if(!state.poseIsInitialised())
+                    state.set(detected_pose.pose, detected_pose.timestamp);
+                else
+                    state.updateFromPosition(detected_pose.pose, detected_pose.timestamp);
+            } 
+            if(!state.poseIsInitialised()) continue;
+
+            auto jvs = velocitizer.multi_area_velocity(sae_handler.getSurface(), tnow, state.query(), 20);
+            state.updateFromVelocity(jvs, tnow);
+
+            { //ROS OUTPUT
                 // format skeleton to ros output
                 yarp::rosmsg::NC_humanPose& ros_output = ros_publisher.prepare();
+                hpecore::skeleton13 pose = state.query();
                 ros_output.pose.resize(26);
                 ros_output.velocity.resize(26, 0.0);
                 for (int j = 0; j < 13; j++)
                 {
-                    ros_output.pose[j*2] = skeleton_detection[j].u;
-                    ros_output.pose[j*2+1] = skeleton_detection[j].v;
+                    ros_output.pose[j*2] = pose[j].u;
+                    ros_output.pose[j*2+1] = pose[j].v;
                 }
                 ros_output.timestamp = tnow;
                 ros_publisher.write();
             }
-
-        } else {
-            yarp::os::Time::delay(0.05);
-        }
-
-
         }
     }
 
