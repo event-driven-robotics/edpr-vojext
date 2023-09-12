1bed21a4084f24f6955655a7c6e615d5f2a3b75e
Arren Glover <arren.glover@iit.it>
2023-09-12 12:08:40 +0000

    update vojext run script

--- start_vojext.sh
+++ start_vojext.sh
@@ -1,35 +1,10 @@
 #!/bin/bash
 
-while getopts 'dm:' OPTION; do
-    case "$OPTION" in
-    d)
-        echo "ROS_MASTER_URI set to default value"
-        export ROS_MASTER_URI=http://127.0.0.1:11311
-        echo "ROS_MASTER_URI="$ROS_MASTER_URI
-        ;;
-    m)
-        echo "ROS_MASTER_URI set manually"
-        export ROS_MASTER_URI="$OPTARG"
-        echo "ROS_MASTER_URI="$ROS_MASTER_URI
-        ;;
-    ?)
-        echo "script usage: /run_vojext.sh [-d (default)] or [-m value (manual)]" >&2
-        exit 1
-        ;;
-    esac
-done
-
-if [ "$#" -eq  "0" ]
-   then
-    echo "ROS_MASTER_URI not set"
-    exit 1
-fi
-
 echo "Run YARP server connected to ROS"
 yarpserver --ros &
-sleep 2
+sleep 1
 echo "Run ATIS-bridge"
-atis-bridge-sdk --s 60 --filter 0.01 &
+atis-bridge-sdk --s 60 --filter 0.01 --limit 2 &
 sleep 5
 echo "Run EDPR-VOJEXT application"
-edpr-vojext-demo --moveEnet_f 5 --publish_f 10 --vis true &
+edpr-vojext-demo --vis false &
