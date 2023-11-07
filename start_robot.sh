4b096985e380821491903408d15dc82cdb2ff8a7
Arren Glover <arrenglover@users.noreply.github.com>
2023-11-07 14:00:59 +0100

    Update and rename start-robot.sh to start_robot.sh

--- start_robot.sh
+++ start_robot.sh
@@ -4,4 +4,4 @@ yarp conf ${YARPSERVER_IP} 10000
 yarpserver --ros &
 sleep 1
 echo "Run ATIS-bridge"
-atis-bridge-sdk --gen3 --s ${P_CAMS} --filter ${P_CAMF} --limit ${P_CAML} &
+atis-bridge-sdk --gen3 --s ${P_CAMS} --filter ${P_CAMF} --limit ${P_CAML} 
