5604bd67d21f79fc6111dc6572b3d2b3d4c5af07
Gaurvi Goyal <gaurvigoyal@gmail.com>
2024-01-22 18:43:06 +0100

    test

--- start_server.sh
+++ start_server.sh
@@ -7,5 +7,5 @@ yarp detect
 sleep 5
 echo "Run EDPR-VOJEXT application"
 edpr-vojext-demo --vis false --f_det ${P_DETF} --f_vis ${P_VISF} --f_vel ${P_VELF} --f_img ${P_IMGF} --confidence ${P_CONF} &
-sleep 1
-python3 /usr/local/src/edpr-vojext/movenet_vojext.py
+#sleep 1
+#python3 /usr/local/src/edpr-vojext/movenet_vojext.py
